0f595edf8ac5de809b113980146936f0176bd179
Fábio Ferreira <fabiohcf@gmail.com>
2023-10-02 11:36:24 -0300

    update

--- desafio047.py
+++ desafio047.py
@@ -1,4 +1,7 @@
 # mostrar todos os números pares entre 1 e 50
-for c in range(1,51):
-    if c%2 == 0:
-        print(c)
+for c in range(1, 51):
+    if c % 2 == 0:
+        print(c, end=', ')
+print()
+for c in range(2,51,2):
+    print(c, end=', ')
